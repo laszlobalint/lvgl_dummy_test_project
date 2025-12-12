57a616cd44999daf351b88ef38ea9d074855a887
Balint Laszlo <balint@lvgl.io>
2025-12-12 14:57:17 +0100

    chore

---
+++ reference_project/preview-build/project/CMakeFiles/lib-ui.dir/DependInfo.cmake
@@ -0,0 +1,61 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/lved/project/components/chart_gen.c" "project/CMakeFiles/lib-ui.dir/components/chart_gen.c.o" "gcc" "project/CMakeFiles/lib-ui.dir/components/chart_gen.c.o.d"
+  "/lved/project/components/component_with_previews/component_with_previews_gen.c" "project/CMakeFiles/lib-ui.dir/components/component_with_previews/component_with_previews_gen.c.o" "gcc" "project/CMakeFiles/lib-ui.dir/components/component_with_previews/component_with_previews_gen.c.o.d"
+  "/lved/project/components/extends_widget_gen.c" "project/CMakeFiles/lib-ui.dir/components/extends_widget_gen.c.o" "gcc" "project/CMakeFiles/lib-ui.dir/components/extends_widget_gen.c.o.d"
+  "/lved/project/components/h3_gen.c" "project/CMakeFiles/lib-ui.dir/components/h3_gen.c.o" "gcc" "project/CMakeFiles/lib-ui.dir/components/h3_gen.c.o.d"
+  "/lved/project/components/my_animated_button_gen.c" "project/CMakeFiles/lib-ui.dir/components/my_animated_button_gen.c.o" "gcc" "project/CMakeFiles/lib-ui.dir/components/my_animated_button_gen.c.o.d"
+  "/lved/project/components/my_animated_list_item_gen.c" "project/CMakeFiles/lib-ui.dir/components/my_animated_list_item_gen.c.o" "gcc" "project/CMakeFiles/lib-ui.dir/components/my_animated_list_item_gen.c.o.d"
+  "/lved/project/components/mybutton_gen.c" "project/CMakeFiles/lib-ui.dir/components/mybutton_gen.c.o" "gcc" "project/CMakeFiles/lib-ui.dir/components/mybutton_gen.c.o.d"
+  "/lved/project/components/super_button_gen.c" "project/CMakeFiles/lib-ui.dir/components/super_button_gen.c.o" "gcc" "project/CMakeFiles/lib-ui.dir/components/super_button_gen.c.o.d"
+  "/lved/project/fonts/Nunito_SemiBold_ttf_data.c" "project/CMakeFiles/lib-ui.dir/fonts/Nunito_SemiBold_ttf_data.c.o" "gcc" "project/CMakeFiles/lib-ui.dir/fonts/Nunito_SemiBold_ttf_data.c.o.d"
+  "/lved/project/fonts/font_sm_data.c" "project/CMakeFiles/lib-ui.dir/fonts/font_sm_data.c.o" "gcc" "project/CMakeFiles/lib-ui.dir/fonts/font_sm_data.c.o.d"
+  "/lved/project/images/img_bell_data.c" "project/CMakeFiles/lib-ui.dir/images/img_bell_data.c.o" "gcc" "project/CMakeFiles/lib-ui.dir/images/img_bell_data.c.o.d"
+  "/lved/project/reference_project.c" "project/CMakeFiles/lib-ui.dir/reference_project.c.o" "gcc" "project/CMakeFiles/lib-ui.dir/reference_project.c.o.d"
+  "/lved/project/reference_project_gen.c" "project/CMakeFiles/lib-ui.dir/reference_project_gen.c.o" "gcc" "project/CMakeFiles/lib-ui.dir/reference_project_gen.c.o.d"
+  "/lved/project/screens/a_permanent_screen_gen.c" "project/CMakeFiles/lib-ui.dir/screens/a_permanent_screen_gen.c.o" "gcc" "project/CMakeFiles/lib-ui.dir/screens/a_permanent_screen_gen.c.o.d"
+  "/lved/project/screens/animations_screen_gen.c" "project/CMakeFiles/lib-ui.dir/screens/animations_screen_gen.c.o" "gcc" "project/CMakeFiles/lib-ui.dir/screens/animations_screen_gen.c.o.d"
+  "/lved/project/screens/main_screen_gen.c" "project/CMakeFiles/lib-ui.dir/screens/main_screen_gen.c.o" "gcc" "project/CMakeFiles/lib-ui.dir/screens/main_screen_gen.c.o.d"
+  "/lved/project/screens/screen_extends_something_gen.c" "project/CMakeFiles/lib-ui.dir/screens/screen_extends_something_gen.c.o" "gcc" "project/CMakeFiles/lib-ui.dir/screens/screen_extends_something_gen.c.o.d"
+  "/lved/project/screens/some_screen_1_gen.c" "project/CMakeFiles/lib-ui.dir/screens/some_screen_1_gen.c.o" "gcc" "project/CMakeFiles/lib-ui.dir/screens/some_screen_1_gen.c.o.d"
+  "/lved/project/screens/test_widgets_gen.c" "project/CMakeFiles/lib-ui.dir/screens/test_widgets_gen.c.o" "gcc" "project/CMakeFiles/lib-ui.dir/screens/test_widgets_gen.c.o.d"
+  "/lved/project/widgets/extends_another/extends_another.c" "project/CMakeFiles/lib-ui.dir/widgets/extends_another/extends_another.c.o" "gcc" "project/CMakeFiles/lib-ui.dir/widgets/extends_another/extends_another.c.o.d"
+  "/lved/project/widgets/extends_another/extends_another_gen.c" "project/CMakeFiles/lib-ui.dir/widgets/extends_another/extends_another_gen.c.o" "gcc" "project/CMakeFiles/lib-ui.dir/widgets/extends_another/extends_another_gen.c.o.d"
+  "/lved/project/widgets/extends_another/extends_another_xml_parser.c" "project/CMakeFiles/lib-ui.dir/widgets/extends_another/extends_another_xml_parser.c.o" "gcc" "project/CMakeFiles/lib-ui.dir/widgets/extends_another/extends_another_xml_parser.c.o.d"
+  "/lved/project/widgets/myarc/myarc.c" "project/CMakeFiles/lib-ui.dir/widgets/myarc/myarc.c.o" "gcc" "project/CMakeFiles/lib-ui.dir/widgets/myarc/myarc.c.o.d"
+  "/lved/project/widgets/myarc/myarc_gen.c" "project/CMakeFiles/lib-ui.dir/widgets/myarc/myarc_gen.c.o" "gcc" "project/CMakeFiles/lib-ui.dir/widgets/myarc/myarc_gen.c.o.d"
+  "/lved/project/widgets/myarc/myarc_xml_parser.c" "project/CMakeFiles/lib-ui.dir/widgets/myarc/myarc_xml_parser.c.o" "gcc" "project/CMakeFiles/lib-ui.dir/widgets/myarc/myarc_xml_parser.c.o.d"
+  "/lved/project/widgets/set_element/set_element.c" "project/CMakeFiles/lib-ui.dir/widgets/set_element/set_element.c.o" "gcc" "project/CMakeFiles/lib-ui.dir/widgets/set_element/set_element.c.o.d"
+  "/lved/project/widgets/set_element/set_element_gen.c" "project/CMakeFiles/lib-ui.dir/widgets/set_element/set_element_gen.c.o" "gcc" "project/CMakeFiles/lib-ui.dir/widgets/set_element/set_element_gen.c.o.d"
+  "/lved/project/widgets/set_element/set_element_xml_parser.c" "project/CMakeFiles/lib-ui.dir/widgets/set_element/set_element_xml_parser.c.o" "gcc" "project/CMakeFiles/lib-ui.dir/widgets/set_element/set_element_xml_parser.c.o.d"
+  "/lved/project/widgets/smart_slider/smart_slider.c" "project/CMakeFiles/lib-ui.dir/widgets/smart_slider/smart_slider.c.o" "gcc" "project/CMakeFiles/lib-ui.dir/widgets/smart_slider/smart_slider.c.o.d"
+  "/lved/project/widgets/smart_slider/smart_slider_gen.c" "project/CMakeFiles/lib-ui.dir/widgets/smart_slider/smart_slider_gen.c.o" "gcc" "project/CMakeFiles/lib-ui.dir/widgets/smart_slider/smart_slider_gen.c.o.d"
+  "/lved/project/widgets/smart_slider/smart_slider_xml_parser.c" "project/CMakeFiles/lib-ui.dir/widgets/smart_slider/smart_slider_xml_parser.c.o" "gcc" "project/CMakeFiles/lib-ui.dir/widgets/smart_slider/smart_slider_xml_parser.c.o.d"
+  "/lved/project/widgets/super_nested/super_nested.c" "project/CMakeFiles/lib-ui.dir/widgets/super_nested/super_nested.c.o" "gcc" "project/CMakeFiles/lib-ui.dir/widgets/super_nested/super_nested.c.o.d"
+  "/lved/project/widgets/super_nested/super_nested_gen.c" "project/CMakeFiles/lib-ui.dir/widgets/super_nested/super_nested_gen.c.o" "gcc" "project/CMakeFiles/lib-ui.dir/widgets/super_nested/super_nested_gen.c.o.d"
+  "/lved/project/widgets/super_nested/super_nested_xml_parser.c" "project/CMakeFiles/lib-ui.dir/widgets/super_nested/super_nested_xml_parser.c.o" "gcc" "project/CMakeFiles/lib-ui.dir/widgets/super_nested/super_nested_xml_parser.c.o.d"
+  "/lved/project/widgets/super_nested_components/super_nested_components.c" "project/CMakeFiles/lib-ui.dir/widgets/super_nested_components/super_nested_components.c.o" "gcc" "project/CMakeFiles/lib-ui.dir/widgets/super_nested_components/super_nested_components.c.o.d"
+  "/lved/project/widgets/super_nested_components/super_nested_components_gen.c" "project/CMakeFiles/lib-ui.dir/widgets/super_nested_components/super_nested_components_gen.c.o" "gcc" "project/CMakeFiles/lib-ui.dir/widgets/super_nested_components/super_nested_components_gen.c.o.d"
+  "/lved/project/widgets/super_nested_components/super_nested_components_xml_parser.c" "project/CMakeFiles/lib-ui.dir/widgets/super_nested_components/super_nested_components_xml_parser.c.o" "gcc" "project/CMakeFiles/lib-ui.dir/widgets/super_nested_components/super_nested_components_xml_parser.c.o.d"
+  "/lved/project/widgets/very_simple/very_simple.c" "project/CMakeFiles/lib-ui.dir/widgets/very_simple/very_simple.c.o" "gcc" "project/CMakeFiles/lib-ui.dir/widgets/very_simple/very_simple.c.o.d"
+  "/lved/project/widgets/very_simple/very_simple_gen.c" "project/CMakeFiles/lib-ui.dir/widgets/very_simple/very_simple_gen.c.o" "gcc" "project/CMakeFiles/lib-ui.dir/widgets/very_simple/very_simple_gen.c.o.d"
+  "/lved/project/widgets/very_simple/very_simple_xml_parser.c" "project/CMakeFiles/lib-ui.dir/widgets/very_simple/very_simple_xml_parser.c.o" "gcc" "project/CMakeFiles/lib-ui.dir/widgets/very_simple/very_simple_xml_parser.c.o.d"
+  "/lved/project/widgets/with_getter/with_getter.c" "project/CMakeFiles/lib-ui.dir/widgets/with_getter/with_getter.c.o" "gcc" "project/CMakeFiles/lib-ui.dir/widgets/with_getter/with_getter.c.o.d"
+  "/lved/project/widgets/with_getter/with_getter_gen.c" "project/CMakeFiles/lib-ui.dir/widgets/with_getter/with_getter_gen.c.o" "gcc" "project/CMakeFiles/lib-ui.dir/widgets/with_getter/with_getter_gen.c.o.d"
+  "/lved/project/widgets/with_getter/with_getter_xml_parser.c" "project/CMakeFiles/lib-ui.dir/widgets/with_getter/with_getter_xml_parser.c.o" "gcc" "project/CMakeFiles/lib-ui.dir/widgets/with_getter/with_getter_xml_parser.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
