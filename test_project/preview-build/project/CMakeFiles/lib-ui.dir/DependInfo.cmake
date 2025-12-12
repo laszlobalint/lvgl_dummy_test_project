a0a287419b072da86f3131e56fbefa872759f263
Balint Laszlo <balint@lvgl.io>
2025-12-12 15:00:05 +0100

    chore

---
+++ test_project/preview-build/project/CMakeFiles/lib-ui.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/lved/project/components/h1_gen.c" "project/CMakeFiles/lib-ui.dir/components/h1_gen.c.o" "gcc" "project/CMakeFiles/lib-ui.dir/components/h1_gen.c.o.d"
+  "/lved/project/components/h2_gen.c" "project/CMakeFiles/lib-ui.dir/components/h2_gen.c.o" "gcc" "project/CMakeFiles/lib-ui.dir/components/h2_gen.c.o.d"
+  "/lved/project/components/h3_gen.c" "project/CMakeFiles/lib-ui.dir/components/h3_gen.c.o" "gcc" "project/CMakeFiles/lib-ui.dir/components/h3_gen.c.o.d"
+  "/lved/project/components/my_button/my_button_gen.c" "project/CMakeFiles/lib-ui.dir/components/my_button/my_button_gen.c.o" "gcc" "project/CMakeFiles/lib-ui.dir/components/my_button/my_button_gen.c.o.d"
+  "/lved/project/fonts/noto_regular_16_data.c" "project/CMakeFiles/lib-ui.dir/fonts/noto_regular_16_data.c.o" "gcc" "project/CMakeFiles/lib-ui.dir/fonts/noto_regular_16_data.c.o.d"
+  "/lved/project/fonts/noto_regular_24_data.c" "project/CMakeFiles/lib-ui.dir/fonts/noto_regular_24_data.c.o" "gcc" "project/CMakeFiles/lib-ui.dir/fonts/noto_regular_24_data.c.o.d"
+  "/lved/project/fonts/noto_regular_32_data.c" "project/CMakeFiles/lib-ui.dir/fonts/noto_regular_32_data.c.o" "gcc" "project/CMakeFiles/lib-ui.dir/fonts/noto_regular_32_data.c.o.d"
+  "/lved/project/screens/screen1/screen1_gen.c" "project/CMakeFiles/lib-ui.dir/screens/screen1/screen1_gen.c.o" "gcc" "project/CMakeFiles/lib-ui.dir/screens/screen1/screen1_gen.c.o.d"
+  "/lved/project/test_project.c" "project/CMakeFiles/lib-ui.dir/test_project.c.o" "gcc" "project/CMakeFiles/lib-ui.dir/test_project.c.o.d"
+  "/lved/project/test_project_gen.c" "project/CMakeFiles/lib-ui.dir/test_project_gen.c.o" "gcc" "project/CMakeFiles/lib-ui.dir/test_project_gen.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ test_project/preview-build/project/CMakeFiles/lib-ui.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lib-ui.dir/components/h1_gen.c.o"
+  "CMakeFiles/lib-ui.dir/components/h1_gen.c.o.d"
+  "CMakeFiles/lib-ui.dir/components/h2_gen.c.o"
+  "CMakeFiles/lib-ui.dir/components/h2_gen.c.o.d"
+  "CMakeFiles/lib-ui.dir/components/h3_gen.c.o"
+  "CMakeFiles/lib-ui.dir/components/h3_gen.c.o.d"
+  "CMakeFiles/lib-ui.dir/components/my_button/my_button_gen.c.o"
+  "CMakeFiles/lib-ui.dir/components/my_button/my_button_gen.c.o.d"
+  "CMakeFiles/lib-ui.dir/fonts/noto_regular_16_data.c.o"
+  "CMakeFiles/lib-ui.dir/fonts/noto_regular_16_data.c.o.d"
+  "CMakeFiles/lib-ui.dir/fonts/noto_regular_24_data.c.o"
+  "CMakeFiles/lib-ui.dir/fonts/noto_regular_24_data.c.o.d"
+  "CMakeFiles/lib-ui.dir/fonts/noto_regular_32_data.c.o"
+  "CMakeFiles/lib-ui.dir/fonts/noto_regular_32_data.c.o.d"
+  "CMakeFiles/lib-ui.dir/screens/screen1/screen1_gen.c.o"
+  "CMakeFiles/lib-ui.dir/screens/screen1/screen1_gen.c.o.d"
+  "CMakeFiles/lib-ui.dir/test_project.c.o"
+  "CMakeFiles/lib-ui.dir/test_project.c.o.d"
+  "CMakeFiles/lib-ui.dir/test_project_gen.c.o"
+  "CMakeFiles/lib-ui.dir/test_project_gen.c.o.d"
+  "liblib-ui.a"
+  "liblib-ui.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lib-ui.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
